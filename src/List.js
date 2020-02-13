b4df6470d0f341a3e6a8bf75897481cd4277cc1b
Martyna <marsze007@wp.pl>
2020-02-13 06:50:19 +0100

    Add search bar

--- src/List.js
+++ src/List.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Product from './Product'
+import SearchInput, { createFilter } from 'react-search-input'
+
+const KEYS_TO_FILTERS = ['name', 'category', 'tags', 'alternatives.zeroWaste', 'alternatives.ecoFriendly']
 
 class List extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            products: []
+            products: [],
+            searchTerm: ''
         };
+        this.searchUpdated = this.searchUpdated.bind(this)
     }
     componentDidMount() {
         this.setState({
@@ -14,16 +19,25 @@ class List extends React.Component {
         })
     }
 
-    render() {
-        return (
-            <div className="list-container">
-                {this.state.products.map((product, index) =>
-                    <Product
-                        key={index}
-                        product={product}
-                    />
-                )}
+    searchUpdated(term) {
+        this.setState({ searchTerm: term })
+    }
 
+    render() {
+        const filteredProducts = this.state.products.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+
+        return (
+            <div>
+                <SearchInput className="search-input" data={this.state.products} onChange={this.searchUpdated} />
+                <div className="list-container">
+                    {filteredProducts.map((product, index) =>
+                        <Product
+                            key={index}
+                            product={product}
+                        />
+                    )}
+
+                </div>
             </div>
         )
     }
